dd84f3bead5bf0e076caba7f9da6af6ab0743cfc
gerasem <sougift@gmail.com>
2021-08-16 01:44:26 +0200

    vue TestComponent; npm run watch

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        lorem ipsum
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
